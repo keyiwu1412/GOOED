4e663141e723fd38c469aad4b3287e11738b36b9
Keyi Wu <42977462+keyiwu1412@users.noreply.github.com>
2022-06-08 10:29:40 -0500

    updates

--- hippylib/oed/ReducedHessian_J.py
+++ hippylib/oed/ReducedHessian_J.py
@@ -1,15 +1,7 @@
-# Copyright (c) 2016-2018, The University of Texas at Austin 
-# & University of California, Merced.
-#
-# All Rights reserved.
-# See file COPYRIGHT for details.
-#
-# This file is part of the hIPPYlib library. For more information and source code
-# availability see https://hippylib.github.io.
-#
-# hIPPYlib is free software; you can redistribute it and/or modify it under the
-# terms of the GNU General Public License (as published by the Free
-# Software Foundation) version 2.0 dated June 1991.
+# Author: Keyi Wu 
+# keyiwu@math.texas.edu
+# University of Texas at Austin
+
 
 from __future__ import absolute_import, division, print_function
 
